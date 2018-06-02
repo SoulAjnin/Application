1bdd1d6a401d5301ff0ba3d4bacaa0fd2ff13430
Philippe <philippechevreul97@orange.fr>
2018-06-02 13:26:43 +0100

    pour loan <3

--- index.php
+++ index.php
@@ -1,13 +1,38 @@
  <?php include('inc/header.html'); ?>
 
+		<div class="page">
+			<div id="tab-1" class="tab-content active">
+ 				<div class="main-content">
+					 <div class="ligne">
+						 <h1 class="titreLigne">Renseigner la ligne de production</h1>
+							<select class="dropdown">
+								<option class="disabled-option" value="" disabled selected hidden>Choisir...</option>
+								<option value="Ligne B">Ligne B</option>
+								<option value="Ligne C">Ligne C</option>
+							</select>
+						</div>
+					</div>
+					<aside class="typeAround">
+						<section class="file-marker">
+							<div>
+									<div class="box-title">
+											Informations
+									</div>
+									<div class="box-contents">
+											<div class="audit-trail">
+												Salut c'est cool 
+											</div>
 
-		<div id="tab-1" class="tab-content active">coucou
+									</div>
+							</div>
+						</section>
+					</aside>
 				</div>
-		
-		<div id="tab-2" class="tab-content">CSS stands for Cascading Style Sheets. CSS describes how HTML elements are to be displayed on screen, paper, or in other media. CSS saves a lot of work. It can control the layout of multiple web pages all at once.</div>
+			
+			<div id="tab-2" class="tab-content">CSS stands for Cascading Style Sheets. CSS describes how HTML elements are to be displayed on screen, paper, or in other media. CSS saves a lot of work. It can control the layout of multiple web pages all at once.</div>
 
-		<div id="tab-3"class="tab-content">jQuery is a fast, small, and feature-rich JavaScript library. It makes things like HTML document traversal and manipulation, event handling, animation, and Ajax much simpler with an easy-to-use API that works across a multitude of browsers.</div>
+			<div id="tab-3"class="tab-content">jQuery is a fast, small, and feature-rich JavaScript library. It makes things like HTML document traversal and manipulation, event handling, animation, and Ajax much simpler with an easy-to-use API that works across a multitude of browsers.</div>
 
+		</div>
 	</div>
-
  <?php include('inc/footer.html'); ?>
